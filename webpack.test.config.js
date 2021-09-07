76496de74cff17c7bc52447e3e50c2597db3493d
Daniel Petranek <dpetranek@flur.ee>
2021-09-07 14:00:47 -0500

    Fix cljs tests
    We need to explicitly disallow polyfills for the node dependencies - we don't need them
    in the browser.

---
+++ webpack.test.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  entry: "./out/test/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./out/test/"),
+  },
+  resolve: {
+    fallback: {
+      crypto: false
+    }
+  }
+};
