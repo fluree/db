c77c056b407dd85df73f17ae4ce282722ff98640
Benjamin Lamothe <bl@732.one>
2021-10-03 10:34:16 -0400

    refactor full text tests for robustness

--- test/fluree/db/full_text_test.clj
+++ test/fluree/db/full_text_test.clj
@@ -10,28 +10,35 @@
     (let [lang :en]
       (with-open [idx (full-text/memory-index lang)]
         (testing "after initialization"
+          (doto (full-text/writer idx)
+            .commit
+            .close)
           (testing "populated with predicates for a subject"
             (let [cid       12345
                   subj-num  10
                   subj-id   (flake/->sid cid subj-num)
-                  pred-vals {1001 "foo", 1002 "bar"}]
-              (with-open [wrtr (doto (full-text/writer idx)
-                                 .commit)]
+                  text-1    "it is raining right now"
+                  text-2    "the sun is shining"
+                  pred-vals {1001 text-1, 1002 text-2}]
+              (with-open [wrtr (full-text/writer idx)]
                 (full-text/put-subject idx wrtr subj-id pred-vals)
                 (.commit wrtr)
                 (let [subject-under-test (full-text/get-subject idx subj-id)]
-                  (is (and (= "foo" (:1001 subject-under-test))
-                           (= "bar" (:1002 subject-under-test)))
+                  (is (and (= text-1 (:1001 subject-under-test))
+                           (= text-2 (:1002 subject-under-test)))
                       "populated subject can be retrieved")))
+              (testing "search"
+                )
               (testing "when updating a single predicate"
-                (let [pred-update {1001 "baz"}]
+                (let [text-update "the rain has stopped"
+                      pred-update {1001 text-update}]
                   (with-open [wrtr (full-text/writer idx)]
                     (full-text/put-subject idx wrtr subj-id pred-update)
                     (.commit wrtr)
                     (let [subject-under-test (full-text/get-subject idx subj-id)]
-                      (is (= "baz" (:1001 subject-under-test))
+                      (is (= text-update (:1001 subject-under-test))
                           "the updated predicate can be retrieved")
-                      (is (= "bar" (:1002 subject-under-test))
+                      (is (= text-2 (:1002 subject-under-test))
                           "unchanged predicates are retained")
-                      (is (not (-> subject-under-test vals set (contains? "foo")))
+                      (is (not (-> subject-under-test vals set (contains? text-1)))
                           "previous predicate values are not retained"))))))))))))
