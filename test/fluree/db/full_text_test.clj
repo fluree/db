4d54d1f63c09bf820a7b1b6d86f54c61c59a40b9
Benjamin Lamothe <bl@732.one>
2021-10-04 14:51:14 -0400

    refactor collection and predicate ids outside of test

--- test/fluree/db/full_text_test.clj
+++ test/fluree/db/full_text_test.clj
@@ -1,9 +1,24 @@
 (ns fluree.db.full-text-test
   (:require [clojure.test :refer [deftest is testing]]
+            [fluree.db.constants :as const]
+            [fluree.db.dbproto :refer [IFlureeDb]]
             [fluree.db.flake :as flake]
             [fluree.db.full-text :as full-text])
   (:import (java.io Closeable)
            (org.apache.lucene.index IndexWriter)))
+
+(def user-coll-id (inc const/$numSystemCollections))
+(def user-coll-name "user")
+
+(def handle-pred-id 200)
+(def handle-pred-name "user/handle")
+
+(def bio-pred-id 201)
+(def bio-pred-name "user/bio")
+
+(defn pid->keyword
+  [pid]
+  (-> pid str keyword))
 
 (deftest full-text-index-test
   (testing "full-text index"
@@ -14,31 +29,32 @@
             .commit
             .close)
           (testing "populated with predicates for a subject"
-            (let [cid       12345
-                  subj-num  10
-                  subj-id   (flake/->sid cid subj-num)
-                  text-1    "it is raining right now"
-                  text-2    "the sun is shining"
-                  pred-vals {1001 text-1, 1002 text-2}]
+            (let [subj-num  10
+                  subj-id   (flake/->sid-checked user-coll-id subj-num)
+                  handle    "mfillmore13"
+                  bio-1     "I actually was the president"
+                  pred-vals {handle-pred-id handle, bio-pred-id bio-1}]
               (with-open [wrtr (full-text/writer idx)]
                 (full-text/put-subject idx wrtr subj-id pred-vals)
                 (.commit wrtr)
                 (let [subject-under-test (full-text/get-subject idx subj-id)]
-                  (is (and (= text-1 (:1001 subject-under-test))
-                           (= text-2 (:1002 subject-under-test)))
+                  (is (and (= handle (get subject-under-test
+                                          (pid->keyword handle-pred-id)))
+                           (= bio-1 (get subject-under-test
+                                         (pid->keyword bio-pred-id))))
                       "populated subject can be retrieved")))
-              (testing "search"
-                )
               (testing "when updating a single predicate"
-                (let [text-update "the rain has stopped"
-                      pred-update {1001 text-update}]
+                (let [bio-update  "No really, I was POTUS"
+                      pred-update {bio-pred-id bio-update}]
                   (with-open [wrtr (full-text/writer idx)]
                     (full-text/put-subject idx wrtr subj-id pred-update)
                     (.commit wrtr)
                     (let [subject-under-test (full-text/get-subject idx subj-id)]
-                      (is (= text-update (:1001 subject-under-test))
+                      (is (= bio-update (get subject-under-test
+                                              (pid->keyword bio-pred-id)))
                           "the updated predicate can be retrieved")
-                      (is (= text-2 (:1002 subject-under-test))
+                      (is (= handle (get subject-under-test
+                                         (pid->keyword handle-pred-id)))
                           "unchanged predicates are retained")
-                      (is (not (-> subject-under-test vals set (contains? text-1)))
+                      (is (not (-> subject-under-test vals set (contains? bio-1)))
                           "previous predicate values are not retained"))))))))))))
