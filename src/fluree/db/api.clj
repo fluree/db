755f9150e5776f37494bfd07f41a058bf63eface
Wes Morgan <git@wesmorgan.me>
2022-09-12 12:22:13 -0600

    Fix infinite recursion bug in api/delete-ledger-async

--- src/fluree/db/api.clj
+++ src/fluree/db/api.clj
@@ -328,7 +328,7 @@
   being in a deletion state during the deletion process.
 
   Attempts to use a ledger in a deletion state will throw an exception."
-  ([conn ledger] (delete-ledger-async conn ledger))
+  ([conn ledger] (delete-ledger-async conn ledger nil))
   ([conn ledger opts]
    (try (let [{:keys [nonce expire timeout private-key] :or {timeout 60000}} opts
               timestamp (System/currentTimeMillis)
