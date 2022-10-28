e11362537f1ec01d6580c604b49221daabe67d91
Brian Platz <bplatz@gmail.com>
2022-10-28 08:25:10 -0400

    Update retraction test

--- test/fluree/db/transact/retraction_test.clj
+++ test/fluree/db/transact/retraction_test.clj
@@ -6,33 +6,37 @@
 
 (use-fixtures :once test/test-system)
 
-(deftest using-pre-defined-types-as-classes
-  (testing "Class not used as class initially can still be used as one."
-    (let [conn   test/memory-conn
-          ledger @(fluree/create conn "tx/retract")
-          db1    @(fluree/stage
-                    ledger
-                    {:context      {:ex "http://example.org/ns/"}
-                     :id           :ex/alice,
-                     :type         :ex/User,
-                     :schema/name  "Alice"
-                     :ex/last      "Blah"
-                     :schema/email "alice@example.org"
-                     :schema/age   42
-                     :ex/favNums   [42, 76, 9]
-                     :ex/scores    [102 92.5 90]})
-          db2    @(fluree/stage
-                    db1
-                    {:context    {:ex "http://example.org/ns/"}
-                     :id         :ex/alice,
-                     :schema/age nil})]
-      (is (= @(fluree/query db2 {:context {:ex "http://example.org/ns/"}
-                                 :select  [:*]
-                                 :from    :ex/alice})
+(deftest retracting-data
+  (testing "Retractions of individual properties and entire subjects."
+    (let [conn           test/memory-conn
+          ledger         @(fluree/create conn "tx/retract")
+          db             @(fluree/stage
+                            ledger
+                            {:context {:ex "http://example.org/ns/"}
+                             :graph   [{:id          :ex/alice,
+                                        :type        :ex/User,
+                                        :schema/name "Alice"
+                                        :schema/age  42}
+                                       {:id          :ex/bob,
+                                        :type        :ex/User,
+                                        :schema/name "Bob"
+                                        :schema/age  22}
+                                       {:id          :ex/jane,
+                                        :type        :ex/User,
+                                        :schema/name "Jane"
+                                        :schema/age  30}]})
+          ;; retract Alice's age attribute by using nil
+          db-age-retract @(fluree/stage
+                            db
+                            {:context    {:ex "http://example.org/ns/"}
+                             :id         :ex/alice,
+                             :schema/age nil})]
+      (is (= @(fluree/query db-age-retract
+                            {:context {:ex "http://example.org/ns/"}
+                             :select  [:*]
+                             :from    :ex/alice})
              [{:id           :ex/alice,
                :rdf/type     [:ex/User],
-               :ex/favNums   [9 42 76],
-               :ex/scores    [90 92.5 102],
-               :ex/last      "Blah",
                :schema/email "alice@example.org",
-               :schema/name  "Alice"}])))))
+               :schema/name  "Alice"}])
+          "Alice should no longer have an age property"))))
