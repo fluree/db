8810f6e3323ff08dd2d66c34ec6a8897b5ac3655
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-12-20 00:01:02 -0500

    trying with docker layer cache

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -7,5 +7,5 @@ image=fluree/${PWD##*/}
 echo "Running in ${image} container..."
 
 export DOCKER_BUILDKIT=1
-docker build --quiet --load --tag "${image}" .
+docker build --cache-from type=gha --quiet --load --tag "${image}" .
 docker run --security-opt seccomp=docker-chrome-seccomp.json --rm "${image}" "$@"
