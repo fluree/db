8e477d4f0a798ff00ac5e08d304009622f06c85b
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-12-20 01:03:49 -0500

    try with just --cache-from --cach-to

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -7,5 +7,5 @@ image=fluree/${PWD##*/}
 echo "Running in ${image} container..."
 
 export DOCKER_BUILDKIT=1
-# docker build --cache-from type=gha --quiet --load --tag "${image}" .
+docker build --cache-from type=gha --cache-to type=gha,mode=max --quiet --load --tag "${image}" .
 docker run --security-opt seccomp=docker-chrome-seccomp.json --rm "${image}" "$@"
