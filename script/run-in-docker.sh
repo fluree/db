15476a47bb826a1a8572432619c6c809f96111d3
Wes Morgan <git@wesmorgan.me>
2022-12-22 14:56:20 -0600

    Remove unused GHA conditional in run-in-docker.sh

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -7,7 +7,5 @@ image=fluree/${PWD##*/}
 echo "Running in ${image} container..."
 
 export DOCKER_BUILDKIT=1
-if [ "$GITHUB_ACTIONS" != "true" ]; then
-  docker build --quiet --load --tag "${image}" .
-fi
+docker build --quiet --load --tag "${image}" .
 docker run --security-opt seccomp=docker-chrome-seccomp.json --rm "${image}" "$@"
