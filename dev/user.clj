cfd93f2222072305415e226a67ae9c3492ee0b70
Marcela Poffald <mpoffald@flur.ee>
2023-01-20 15:48:03 -0500

    useful repl workflow

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,8 @@
 (ns user
   (:require [clojure.tools.namespace.repl :as tn :refer [refresh refresh-all]]
             [clojure.core.async :as async]
+            [fluree.db.did :as did]
+            [fluree.db.json-ld.api :as fluree]
             [fluree.db.util.async :refer [<? go-try merge-into?]]
             [fluree.db.flake :as flake]
             [fluree.db.permissions :as permissions]
@@ -18,7 +20,7 @@
             [clojure.string :as str]
             [criterium.core :refer [bench]]
             ;; cljs
-            [figwheel-sidecar.repl-api :as ra]))
+            [figwheel-sidecar.repl-api :as ra])) 
 
 
 ;; async/query todo
@@ -34,6 +36,77 @@
 
 (set! *warn-on-reflection* true)
 
+
+
+(comment
+
+
+
+  (do 
+    (def default-private-key
+      "8ce4eca704d653dec594703c81a84c403c39f262e54ed014ed857438933a2e1c") 
+
+
+    (def did (did/private->did-map default-private-key )) 
+
+    (def default-context
+      {:id     "@id"
+       :type   "@type"
+       :xsd    "http://www.w3.org/2001/XMLSchema#"
+       :rdf    "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+       :rdfs   "http://www.w3.org/2000/01/rdf-schema#"
+       :sh     "http://www.w3.org/ns/shacl#"
+       :schema "http://schema.org/"
+       :skos   "http://www.w3.org/2008/05/skos#"
+       :wiki   "https://www.wikidata.org/wiki/"
+       :f      "https://ns.flur.ee/ledger#"}))  
+
+  (def file-conn @(fluree/connect {:method :file
+                                   :storage-path "dev/data"
+                                   :defaults
+                                   {:context (merge default-context {:ex "http://example.org/ns/"})
+                                  ;;  :did did
+                                    }}))       
+
+
+
+  (def ledger @(fluree/create file-conn "user/test"))  
+
+  (def db @(fluree/stage
+            (fluree/db ledger)
+            [{:context      {:ex "http://example.org/ns/"}
+              :id           :ex/brian,
+              :type         :ex/User,
+              :schema/name  "Brian"
+              :schema/email "brian@example.org"
+              :schema/age   50
+             ;; :ex/favNums   7
+              }
+             {:context      {:ex "http://example.org/ns/"}
+              :id           :ex/alice,
+              :type         :ex/User,
+              :schema/name  "Alice"
+              :schema/email "alice@example.org"
+              :schema/age   50
+              ;;:ex/favNums   [42, 76, 9]
+              }
+             {:context      {:ex "http://example.org/ns/"}  
+              :id           :ex/cam,
+              :type         :ex/User,
+              :schema/name  "Cam"
+              :schema/email "cam@example.org"
+              :schema/age   34
+              ;;:ex/favNums   [5, 10]
+             ;; :ex/friend    [:ex/brian :ex/alice]
+              }]))      
+
+  (def comm @(fluree/commit! ledger db {:message "hi"})) 
+
+  @(fluree/load file-conn (-> ledger :alias)) 
+
+  #_(def details @(fluree/query db {:commit {} }))
+
+  )
 
 ;; TODO - general
 ;; if an subject is a component, do we allow it to be explicitly assigned as a ref to another component?
