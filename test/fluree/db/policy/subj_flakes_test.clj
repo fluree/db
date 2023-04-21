ea1588b69bf20976733fa04221947426f760bd64
Wes Morgan <git@wesmorgan.me>
2023-04-21 09:23:59 -0600

    Stringify new policy subj-flakes test

--- test/fluree/db/policy/subj_flakes_test.clj
+++ test/fluree/db/policy/subj_flakes_test.clj
@@ -1,11 +1,12 @@
 (ns fluree.db.policy.subj-flakes-test
   (:require
-    [clojure.test :refer :all]
-    [fluree.db.test-utils :as test-utils]
-    [fluree.db.json-ld.api :as fluree]
-    [fluree.db.did :as did]
-    [fluree.db.permissions-validate :as policy-enforce]
-    [clojure.core.async :as async]))
+   [clojure.test :refer :all]
+   [fluree.db.test-utils :as test-utils]
+   [fluree.db.json-ld.api :as fluree]
+   [fluree.db.did :as did]
+   [fluree.db.permissions-validate :as policy-enforce]
+   [clojure.core.async :as async]
+   [fluree.db.util.log :as log]))
 
 ;; tests for the optimized policy filtering for groups of flakes of the same subject
 ;; (used for simple subject crawl)
@@ -18,57 +19,57 @@
           root-did        (:id (did/private->did-map "8ce4eca704d653dec594703c81a84c403c39f262e54ed014ed857438933a2e1c"))
           alice-did       (:id (did/private->did-map "c0459840c334ca9f20c257bed971da88bd9b1b5d4fca69d4e3f4b8504f981c07"))
           db              @(fluree/stage
-                             (fluree/db ledger)
-                             [{"id"               "ex:alice",
-                               "type"             "ex:User",
-                               "schema:name"      "Alice"
-                               "schema:email"     "alice@flur.ee"
-                               "schema:birthDate" "2022-08-17"
-                               "schema:ssn"       "111-11-1111"
-                               "ex:location"      {"ex:state"   "NC"
-                                                   "ex:country" "USA"}}
-                              {"id"               "ex:john",
-                               "type"             "ex:User",
-                               "schema:name"      "John"
-                               "schema:email"     "john@flur.ee"
-                               "schema:birthDate" "2021-08-17"
-                               "schema:ssn"       "888-88-8888"}
-                              {"id"                   "ex:widget",
-                               "type"                 "ex:Product",
-                               "schema:name"          "Widget"
-                               "schema:price"         99.99
-                               "schema:priceCurrency" "USD"}
-                              ;; assign root-did to "ex:rootRole"
-                              {"id"     root-did
-                               "f:role" {"id" "ex:rootRole"}}
-                              ;; assign alice-did to "ex:userRole" and also link the did to "ex:alice" via "ex:user"
-                              {"id"      alice-did
-                               "ex:user" {"id" "ex:alice"}
-                               "f:role"  {"id" "ex:userRole"}}])
+                            (fluree/db ledger)
+                            [{"id"               "ex:alice",
+                              "type"             "ex:User",
+                              "schema:name"      "Alice"
+                              "schema:email"     "alice@flur.ee"
+                              "schema:birthDate" "2022-08-17"
+                              "schema:ssn"       "111-11-1111"
+                              "ex:location"      {"ex:state"   "NC"
+                                                  "ex:country" "USA"}}
+                             {"id"               "ex:john",
+                              "type"             "ex:User",
+                              "schema:name"      "John"
+                              "schema:email"     "john@flur.ee"
+                              "schema:birthDate" "2021-08-17"
+                              "schema:ssn"       "888-88-8888"}
+                             {"id"                   "ex:widget",
+                              "type"                 "ex:Product",
+                              "schema:name"          "Widget"
+                              "schema:price"         99.99
+                              "schema:priceCurrency" "USD"}
+                             ;; assign root-did to "ex:rootRole"
+                             {"id"     root-did
+                              "f:role" {"id" "ex:rootRole"}}
+                             ;; assign alice-did to "ex:userRole" and also link the did to "ex:alice" via "ex:user"
+                             {"id"      alice-did
+                              "ex:user" {"id" "ex:alice"}
+                              "f:role"  {"id" "ex:userRole"}}])
 
           db+policy       @(fluree/stage
                             db
                             ;; add policy targeting "ex:rootRole" that can view and modify everything
-                             [{"id"           "ex:rootPolicy",
-                               "type"         ["f:Policy"],   ;; must be of type "f:Policy", else it won't be treated as a policy
-                               "f:targetNode" {"id" "f:allNodes"} ;; "f:allNodes" special keyword meaning every node (everything)
-                               "f:allow"      [{"id"           "ex:rootAccessAllow"
-                                                "f:targetRole" {"id" "ex:rootRole"} ;; our name for global / root role
-                                                "f:action"     [{"id" "f:view"}
-                                                                {"id" "f:modify"}]}]}
-                              ;; add a policy targeting "ex:userRole" that can see all users, but only SSN if belonging to themselves
-                              {"id"            "ex:UserPolicy",
-                               "type"          ["f:Policy"],
-                               "f:targetClass" {"id" "ex:User"}
-                               "f:allow"       [{"id"           "ex:globalViewAllow"
-                                                 "f:targetRole" {"id" "ex:userRole"} ;; our assigned name for standard user's role (given to Alice above)
-                                                 "f:action"     [{"id" "f:view"}]}]
-                               "f:property"    [{"f:path"  {"id" "schema:ssn"}
-                                                 "f:allow" [{"id"           "ex:ssnViewRule"
-                                                             "f:targetRole" {"id" "ex:userRole"}
-                                                             "f:action"     [{"id" "f:view"}]
-                                                             "f:equals"     {"@list" [{"id" "f:$identity"}
-                                                                                      {"id" "ex:user"}]}}]}]}])
+                            [{"id"           "ex:rootPolicy",
+                              "type"         ["f:Policy"], ;; must be of type "f:Policy", else it won't be treated as a policy
+                              "f:targetNode" {"id" "f:allNodes"} ;; "f:allNodes" special keyword meaning every node (everything)
+                              "f:allow"      [{"id"           "ex:rootAccessAllow"
+                                               "f:targetRole" {"id" "ex:rootRole"} ;; our name for global / root role
+                                               "f:action"     [{"id" "f:view"}
+                                                               {"id" "f:modify"}]}]}
+                             ;; add a policy targeting "ex:userRole" that can see all users, but only SSN if belonging to themselves
+                             {"id"            "ex:UserPolicy",
+                              "type"          ["f:Policy"],
+                              "f:targetClass" {"id" "ex:User"}
+                              "f:allow"       [{"id"           "ex:globalViewAllow"
+                                                "f:targetRole" {"id" "ex:userRole"} ;; our assigned name for standard user's role (given to Alice above)
+                                                "f:action"     [{"id" "f:view"}]}]
+                              "f:property"    [{"f:path"  {"id" "schema:ssn"}
+                                                "f:allow" [{"id"           "ex:ssnViewRule"
+                                                            "f:targetRole" {"id" "ex:userRole"}
+                                                            "f:action"     [{"id" "f:view"}]
+                                                            "f:equals"     {"@list" [{"id" "f:$identity"}
+                                                                                     {"id" "ex:user"}]}}]}]}])
           ;; get a group of flakes that we know will have different permissions for different users.
           john-flakes     @(fluree/range db+policy :spot = ["ex:john"])
           alice-flakes    @(fluree/range db+policy :spot = [(fluree/expand-iri db+policy "ex:alice")])
@@ -115,37 +116,39 @@
 (deftest ^:integration no-subject-flakes-test
   (testing "If no subject flakes return from a select, policy code should not be run to avoid error."
     (let [conn      (test-utils/create-conn)
-          ledger    @(fluree/create conn "policy/b" {:defaultContext ["" {:ex "http://example.org/ns/"}]})
+          ledger    @(fluree/create conn "policy/b"
+                                    {"defaults"
+                                     {"@context" ["" {"ex" "http://example.org/ns/"}]}})
           alice-did (:id (did/private->did-map "c0459840c334ca9f20c257bed971da88bd9b1b5d4fca69d4e3f4b8504f981c07"))
           db        @(fluree/stage
-                       (fluree/db ledger)
-                       [{:id          :ex/alice,
-                         :type        :ex/User,
-                         :schema/name "alice"}
-                        {:id          :ex/john,
-                         :type        :ex/User,
-                         :schema/name "john"}
-                        {:id           :ex/widget,
-                         :type         :ex/Product,
-                         :schema/price 99.99}
-                        {:id      alice-did
-                         :ex/user :ex/alice
-                         :f/role  :ex/userRole}])
+                      (fluree/db ledger)
+                      [{"id"          "ex:alice"
+                        "type"        "ex:User"
+                        "schema:name" "alice"}
+                       {"id"          "ex:john"
+                        "type"        "ex:User"
+                        "schema:name" "john"}
+                       {"id"           "ex:widget",
+                        "type"         "ex:Product",
+                        "schema:price" 99.99}
+                       {"id"      alice-did
+                        "ex:user" {"id" "ex:alice"}
+                        "f:role"  {"id" "ex:userRole"}}])
 
           db+policy @(fluree/stage
-                       db
-                       ;; add policy targeting :ex/rootRole that can view and modify everything
-                       [{:id            :ex/userPolicy,
-                         :type          [:f/Policy],
-                         :f/targetClass :ex/User
-                         :f/allow       [{:id           :ex/globalViewAllow
-                                          :f/targetRole :ex/userRole
-                                          :f/action     [:f/view]}]}])
+                      db
+                      ;; add policy targeting :ex/rootRole that can view and modify everything
+                      [{"id"            "ex:userPolicy",
+                        "type"          ["f:Policy"],
+                        "f:targetClass" {"id" "ex:User"}
+                        "f:allow"       [{"id"           "ex:globalViewAllow"
+                                          "f:targetRole" {"id" "ex:userRole"}
+                                          "f:action"     [{"id" "f:view"}]}]}])
 
-          alice-db  @(fluree/wrap-policy db+policy {:did  alice-did
-                                                    :role :ex/userRole})]
+          alice-db  @(fluree/wrap-policy db+policy {"did" alice-did
+                                                    "role" "ex:userRole"})]
 
       (is (= []
-             @(fluree/query alice-db '{:select {?s [:*]}
-                                       :where  [[?s :schema/price 99.99]]}))
+             @(fluree/query alice-db '{"select" {?s ["*"]}
+                                       "where"  [[?s "schema:price" 99.99]]}))
           "There shouldn't be an exception, just empty vector."))))
