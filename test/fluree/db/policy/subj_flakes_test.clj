3182243f84f4ef31ba3182633150064ee3ce7e2f
Benjamin Lamothe <bl@732.one>
2023-04-21 10:22:12 -0400

    fix defaultContext key

--- test/fluree/db/policy/subj_flakes_test.clj
+++ test/fluree/db/policy/subj_flakes_test.clj
@@ -113,7 +113,7 @@
 (deftest ^:integration no-subject-flakes-test
   (testing "If no subject flakes return from a select, policy code should not be run to avoid error."
     (let [conn      (test-utils/create-conn)
-          ledger    @(fluree/create conn "policy/b" {:context {:ex "http://example.org/ns/"}})
+          ledger    @(fluree/create conn "policy/b" {:defaultContext ["" {:ex "http://example.org/ns/"}]})
           alice-did (:id (did/private->did-map "c0459840c334ca9f20c257bed971da88bd9b1b5d4fca69d4e3f4b8504f981c07"))
           db        @(fluree/stage
                        (fluree/db ledger)
