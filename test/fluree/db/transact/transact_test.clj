6da2840bbf49114d74508f56a7ba02921c2dd11c
Marcela Poffald <mpoffald@flur.ee>
2023-06-07 16:00:03 -0400

    Ensure objects of `fluree:targetClass` are correctly handled as types, regardless of txn order
    - adds `fluree:targetClass` to list of predefined properties
    - extends check for existence of type flakes to make sure that a flake with predicate `rdf:type` and object `rdfs:Class` is created if necessary.
    
    Previously, we just checked if a subject id for a given type already existed, and if so, did not create any flakes for it. But in the case where a type first appears as the object of `fluree:targetClass`, we would create _only_ an "iri flake", so we would have a subject id, but no flake identifying the type as being of type `rdfs:Class`. With this change, we create this additional flake if necessary.

--- test/fluree/db/transact/transact_test.clj
+++ test/fluree/db/transact/transact_test.clj
@@ -1,5 +1,6 @@
 (ns fluree.db.transact.transact-test
   (:require [clojure.test :refer :all]
+            [fluree.db.did :as did]
             [fluree.db.util.core :as util]
             [fluree.db.test-utils :as test-utils]
             [fluree.db.json-ld.api :as fluree]
@@ -63,3 +64,47 @@
               [:id :id "@id"]]
              @(fluree/query db-bool '{:select  [?s ?p ?o]
                                       :where   [[?s ?p ?o]]}))))))
+
+
+(deftest policy-ordering-test
+  (testing "transaction order does not affect query results"
+    (let [conn            (test-utils/create-conn)
+          ledger          @(fluree/create conn "tx/policy-order" {:defaultContext ["" {:ex "http://example.org/ns/"}]})
+          alice-did       (:id (did/private->did-map "c0459840c334ca9f20c257bed971da88bd9b1b5d4fca69d4e3f4b8504f981c07"))
+          data            [{:id          :ex/alice,
+                            :type        :ex/User,
+                            :schema/name "Alice"}
+                           {:id          :ex/john,
+                            :type        :ex/User,
+                            :schema/name "John"}
+                           {:id      alice-did
+                            :ex/user :ex/alice
+                            :f/role  :ex/userRole}]
+          policy          [{:id            :ex/UserPolicy,
+                            :type          [:f/Policy],
+                            :f/targetClass :ex/User
+                            :f/allow       [{:id           :ex/globalViewAllow
+                                             :f/targetRole :ex/userRole
+                                             :f/action     [:f/view]}]}]
+          db-data-first   @(fluree/stage
+                             (fluree/db ledger)
+                             (into data policy))
+          db-policy-first @(fluree/stage
+                             (fluree/db ledger)
+                             (into policy data))
+          user-query      '{:select {?s [:*]}
+                            :where  [[?s :rdf/type :ex/User]]}
+          class-query     '{:select {?type [:*]}
+                            :where  [[?type :rdf/type :rdfs/Class]]}]
+      (let [users [{:id :ex/john, :rdf/type [:ex/User], :schema/name "John"}
+                   {:id :ex/alice, :rdf/type [:ex/User], :schema/name "Alice"}]]
+        (is (= users
+               @(fluree/query db-data-first user-query)))
+        (is (= users
+               @(fluree/query db-policy-first user-query))))
+      (let [types [{:id :f/Policy, :rdf/type [:rdfs/Class]}
+                   {:id :ex/User, :rdf/type [:rdfs/Class]}]]
+        (is (= (into #{} types)
+               (into #{} @(fluree/query db-data-first class-query))))
+        (is (= (into #{} types)
+               (into #{} @(fluree/query db-policy-first class-query))))))))
