bf51e2a8185edc2c5262a5547280623926c25541
Benjamin Lamothe <bl@732.one>
2023-10-29 16:09:59 -0400

    update json-ld-basic test

--- test/fluree/db/query/json_ld_basic_test.clj
+++ test/fluree/db/query/json_ld_basic_test.clj
@@ -79,19 +79,20 @@
           db     (fluree/db movies)]
       (testing "basic analytical RFD type query"
         (let [query-res @(fluree/query db '{:select {?s [:* {:schema/isBasedOn [:*]}]}
-                                            :where  [[?s :type :schema/Movie]]})]
-          (is (= [{:id :wiki/Q2875,
-                   :type :schema/Movie,
+                                            :where  {:id   ?s
+                                                     :type :schema/Movie}})]
+          (is (= [{:id                               :wiki/Q2875,
+                   :type                             :schema/Movie,
                    :schema/disambiguatingDescription "1939 film by Victor Fleming",
-                   :schema/isBasedOn {:id :wiki/Q2870,
-                                      :type :schema/Book,
-                                      :schema/author {:id :wiki/Q173540},
-                                      :schema/isbn "0-582-41805-4",
-                                      :schema/name "Gone with the Wind"},
-                   :schema/name "Gone with the Wind",
-                   :schema/titleEIDR "10.5240/FB0D-0A93-CAD6-8E8D-80C2-4"}
+                   :schema/isBasedOn                 {:id            :wiki/Q2870,
+                                                      :type          :schema/Book,
+                                                      :schema/author {:id :wiki/Q173540},
+                                                      :schema/isbn   "0-582-41805-4",
+                                                      :schema/name   "Gone with the Wind"},
+                   :schema/name                      "Gone with the Wind",
+                   :schema/titleEIDR                 "10.5240/FB0D-0A93-CAD6-8E8D-80C2-4"}
                   {:id                               :wiki/Q230552,
-                   :type                         :schema/Movie,
+                   :type                             :schema/Movie,
                    :schema/name                      "Back to the Future Part III",
                    :schema/disambiguatingDescription "1990 film by Robert Zemeckis",
                    :schema/titleEIDR                 "10.5240/15F9-F913-FF25-8041-E798-O"}
@@ -100,7 +101,7 @@
                    :schema/titleEIDR  "10.5240/5DA5-C386-2911-7E2B-1782-L",
                    :schema/followedBy {:id :wiki/Q230552}}
                   {:id                               :wiki/Q91540,
-                   :type                         :schema/Movie,
+                   :type                             :schema/Movie,
                    :schema/name                      "Back to the Future",
                    :schema/disambiguatingDescription "1985 film by Robert Zemeckis",
                    :schema/titleEIDR                 "10.5240/09A3-1F6E-3538-DF46-5C6F-I",
@@ -110,7 +111,7 @@
                    :schema/disambiguatingDescription "2005 British-American comic science fiction film directed by Garth Jennings",
                    :schema/titleEIDR                 "10.5240/B752-5B47-DBBE-E5D4-5A3F-N",
                    :schema/isBasedOn                 {:id            :wiki/Q3107329,
-                                                      :type      :schema/Book,
+                                                      :type          :schema/Book,
                                                       :schema/name   "The Hitchhiker's Guide to the Galaxy",
                                                       :schema/isbn   "0-330-25864-8",
                                                       :schema/author {:id :wiki/Q42}}}]                                  ;; :id is a DID and will be unique per DB so exclude from comparison
@@ -130,8 +131,7 @@
                                         :id      "list-test"
                                         :ex/list [42 2 88 1]})
               query-res @(fluree/query db '{:context   ["" {:ex "http://example.org/ns#"}]
-                                            :selectOne {?s [:*]},
-                                            :where     [[?s :id "list-test"]]})]
+                                            :selectOne {"list-test" [:*]}})]
           (is (= {:id      "list-test"
                   :ex/list [42 2 88 1]}
                  query-res)
@@ -143,8 +143,7 @@
                                         :id      "list-test2"
                                         :ex/list {"@list" [42 2 88 1]}})
               query-res @(fluree/query db '{:context   ["" {:ex "http://example.org/ns#"}],
-                                            :selectOne {?s [:*]},
-                                            :where     [[?s :id "list-test2"]]})]
+                                            :selectOne {"list-test2" [:*]},})]
           (is (= {:id      "list-test2"
                   :ex/list [42 2 88 1]}
                  query-res)
@@ -188,112 +187,115 @@
                     :schema/age   46
                     :ex/favNums   [15 70]
                     :ex/friend    [:ex/cam]}])]
-    (testing "using `where`"
-      (testing "id"
-        ;;TODO not getting reparsed as ssc
-        (is (= [{:id           :ex/brian,
-                 :type     :ex/User
-                 :schema/name  "Brian"
-                 :ex/last      "Smith"
-                 :schema/email "brian@example.org"
-                 :schema/age   50
-                 :ex/favColor  "Green"
-                 :ex/favNums   7}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :id :ex/brian]]}))))
+    (testing "direct id"
       ;;TODO not getting reparsed as ssc
-      (testing "iri"
-        (is (= [{:id           :ex/david
-                 :type     :ex/User
-                 :schema/name  "David"
-                 :ex/last      "Jones"
-                 :schema/email "david@example.org"
-                 :schema/age   46
-                 :ex/favNums   [15 70]
-                 :ex/friend    {:id :ex/cam}}
-                {:type     :ex/User
-                 :schema/email "cam@example.org"
-                 :ex/favNums   [5 10]
-                 :schema/age   34
-                 :ex/last      "Jones"
-                 :schema/name  "Cam"
-                 :id           :ex/cam
-                 :ex/friend    [{:id :ex/brian} {:id :ex/alice}]
-                 :ex/favColor  "Blue"}
-                {:id           :ex/alice
-                 :type     :ex/User
-                 :schema/name  "Alice"
-                 :ex/last      "Smith"
-                 :schema/email "alice@example.org"
-                 :schema/age   42
-                 :ex/favNums   [9 42 76]
-                 :ex/favColor  "Green"}
-                {:id           :ex/brian
-                 :type     :ex/User
-                 :schema/name  "Brian"
-                 :ex/last      "Smith"
-                 :schema/email "brian@example.org"
-                 :schema/age   50
-                 :ex/favColor  "Green"
-                 :ex/favNums   7}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :type :ex/User]]}))))
-      (testing "tuple"
-        (is (= [{:id           :ex/alice
-                 :type     :ex/User
-                 :schema/name  "Alice"
-                 :ex/last      "Smith"
-                 :schema/email "alice@example.org"
-                 :schema/age   42
-                 :ex/favNums   [9 42 76]
-                 :ex/favColor  "Green"}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :schema/name "Alice"]]})))
-        (is (= [{:type     :ex/User
-                 :schema/email "cam@example.org"
-                 :ex/favNums   [5 10]
-                 :schema/age   34
-                 :ex/last      "Jones"
-                 :schema/name  "Cam"
-                 :id           :ex/cam
-                 :ex/friend    [{:id :ex/brian} {:id :ex/alice}]
-                 :ex/favColor  "Blue"}
-                {:id           :ex/alice
-                 :type     :ex/User
-                 :schema/name  "Alice"
-                 :ex/last      "Smith"
-                 :schema/email "alice@example.org"
-                 :schema/age   42
-                 :ex/favNums   [9 42 76]
-                 :ex/favColor  "Green"}
-                {:id           :ex/brian,
-                 :type     :ex/User,
-                 :ex/favNums   7,
-                 :ex/favColor  "Green",
-                 :schema/age   50,
-                 :ex/last      "Smith",
-                 :schema/email "brian@example.org",
-                 :schema/name  "Brian"}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :ex/favColor "?color"]]})))
-        (is (= [{:id           :ex/alice
-                 :type     :ex/User
-                 :schema/name  "Alice"
-                 :ex/last      "Smith"
-                 :schema/email "alice@example.org"
-                 :schema/age   42
-                 :ex/favNums   [9 42 76]
-                 :ex/favColor  "Green"}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :schema/age 42]]})))
-        (is (= [{:id           :ex/alice,
-                 :type     :ex/User,
-                 :ex/favNums   [9 42 76],
-                 :ex/favColor  "Green",
-                 :schema/age   42,
-                 :ex/last      "Smith",
-                 :schema/email "alice@example.org",
-                 :schema/name  "Alice"}]
-               @(fluree/query db {:select {"?s" ["*"]}
-                                  :where  [["?s" :schema/age 42]
-                                           ["?s" :ex/favColor "Green"]]})))))))
+      (is (= [{:id           :ex/brian,
+               :type         :ex/User
+               :schema/name  "Brian"
+               :ex/last      "Smith"
+               :schema/email "brian@example.org"
+               :schema/age   50
+               :ex/favColor  "Green"
+               :ex/favNums   7}]
+             @(fluree/query db {:select {:ex/brian ["*"]}}))))
+    ;;TODO not getting reparsed as ssc
+    (testing "iri from `where`"
+      (is (= [{:id           :ex/david
+               :type         :ex/User
+               :schema/name  "David"
+               :ex/last      "Jones"
+               :schema/email "david@example.org"
+               :schema/age   46
+               :ex/favNums   [15 70]
+               :ex/friend    {:id :ex/cam}}
+              {:type         :ex/User
+               :schema/email "cam@example.org"
+               :ex/favNums   [5 10]
+               :schema/age   34
+               :ex/last      "Jones"
+               :schema/name  "Cam"
+               :id           :ex/cam
+               :ex/friend    [{:id :ex/brian} {:id :ex/alice}]
+               :ex/favColor  "Blue"}
+              {:id           :ex/alice
+               :type         :ex/User
+               :schema/name  "Alice"
+               :ex/last      "Smith"
+               :schema/email "alice@example.org"
+               :schema/age   42
+               :ex/favNums   [9 42 76]
+               :ex/favColor  "Green"}
+              {:id           :ex/brian
+               :type         :ex/User
+               :schema/name  "Brian"
+               :ex/last      "Smith"
+               :schema/email "brian@example.org"
+               :schema/age   50
+               :ex/favColor  "Green"
+               :ex/favNums   7}]
+             @(fluree/query db {:select {"?s" ["*"]}
+                                :where  {:id   "?s"
+                                         :type :ex/User}}))))
+    (testing "tuple"
+      (is (= [{:id           :ex/alice
+               :type         :ex/User
+               :schema/name  "Alice"
+               :ex/last      "Smith"
+               :schema/email "alice@example.org"
+               :schema/age   42
+               :ex/favNums   [9 42 76]
+               :ex/favColor  "Green"}]
+             @(fluree/query db {:select {"?s" ["*"]}
+                                :where  {:id          "?s"
+                                         :schema/name "Alice"}})))
+      (is (= [{:type         :ex/User
+               :schema/email "cam@example.org"
+               :ex/favNums   [5 10]
+               :schema/age   34
+               :ex/last      "Jones"
+               :schema/name  "Cam"
+               :id           :ex/cam
+               :ex/friend    [{:id :ex/brian} {:id :ex/alice}]
+               :ex/favColor  "Blue"}
+              {:id           :ex/alice
+               :type         :ex/User
+               :schema/name  "Alice"
+               :ex/last      "Smith"
+               :schema/email "alice@example.org"
+               :schema/age   42
+               :ex/favNums   [9 42 76]
+               :ex/favColor  "Green"}
+              {:id           :ex/brian,
+               :type         :ex/User,
+               :ex/favNums   7,
+               :ex/favColor  "Green",
+               :schema/age   50,
+               :ex/last      "Smith",
+               :schema/email "brian@example.org",
+               :schema/name  "Brian"}]
+             @(fluree/query db {:select {"?s" ["*"]}
+                                :where  {:id          "?s"
+                                         :ex/favColor "?color"}})))
+      (is (= [{:id           :ex/alice
+               :type         :ex/User
+               :schema/name  "Alice"
+               :ex/last      "Smith"
+               :schema/email "alice@example.org"
+               :schema/age   42
+               :ex/favNums   [9 42 76]
+               :ex/favColor  "Green"}]
+             @(fluree/query db {:select {"?s" ["*"]}
+                                :where  {:id         "?s"
+                                         :schema/age 42}})))
+      (is (= [{:id           :ex/alice,
+               :type         :ex/User,
+               :ex/favNums   [9 42 76],
+               :ex/favColor  "Green",
+               :schema/age   42,
+               :ex/last      "Smith",
+               :schema/email "alice@example.org",
+               :schema/name  "Alice"}]
+             @(fluree/query db {:select {"?s" ["*"]}
+                                :where  {:id          "?s"
+                                         :schema/age  42
+                                         :ex/favColor "Green"}}))))))
