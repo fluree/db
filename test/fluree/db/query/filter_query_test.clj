5ee80b40e0ab471a36c11275d97e99c1b8d926fe
Benjamin Lamothe <bl@732.one>
2023-12-05 05:04:07 -0500

    remove default context from filter query test

--- test/fluree/db/query/filter_query_test.clj
+++ test/fluree/db/query/filter_query_test.clj
@@ -1,15 +1,16 @@
 (ns fluree.db.query.filter-query-test
-  (:require
-    [clojure.test :refer :all]
-    [fluree.db.test-utils :as test-utils]
-    [fluree.db.json-ld.api :as fluree]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [fluree.db.test-utils :as test-utils]
+            [fluree.db.json-ld.api :as fluree]))
 
 (deftest ^:integration filter-test
   (let [conn   (test-utils/create-conn)
-        ledger @(fluree/create conn "query/filter" {:defaultContext ["" {:ex "http://example.org/ns/"}]})
+        ledger @(fluree/create conn "query/filter")
         db     @(fluree/stage
                   (fluree/db ledger)
-                  {"@context" "https://ns.flur.ee"
+                  {"@context" ["https://ns.flur.ee"
+                               test-utils/default-context
+                               {:ex "http://example.org/ns/"}]
                    "insert"
                    [{:id           :ex/brian,
                      :type         :ex/User,
@@ -26,97 +27,111 @@
                      :ex/favColor  "Green"
                      :schema/age   42
                      :ex/favNums   [42, 76, 9]}
-                    {:id          :ex/cam,
-                     :type        :ex/User,
-                     :schema/name "Cam"
-                     :ex/last     "Jones"
-                     :schema/email    "cam@example.org"
-                     :schema/age  34
-                     :ex/favColor "Blue"
-                     :ex/favNums  [5, 10]
-                     :ex/friend   [:ex/brian :ex/alice]}
-                    {:id          :ex/david,
-                     :type        :ex/User,
-                     :schema/name "David"
-                     :ex/last     "Jones"
-                     :schema/email    "david@example.org"
-                     :schema/age  46
-                     :ex/favNums  [15 70]
-                     :ex/friend   [:ex/cam]}]})]
+                    {:id           :ex/cam,
+                     :type         :ex/User,
+                     :schema/name  "Cam"
+                     :ex/last      "Jones"
+                     :schema/email "cam@example.org"
+                     :schema/age   34
+                     :ex/favColor  "Blue"
+                     :ex/favNums   [5, 10]
+                     :ex/friend    [:ex/brian :ex/alice]}
+                    {:id           :ex/david,
+                     :type         :ex/User,
+                     :schema/name  "David"
+                     :ex/last      "Jones"
+                     :schema/email "david@example.org"
+                     :schema/age   46
+                     :ex/favNums   [15 70]
+                     :ex/friend    [:ex/cam]}]})]
 
     (testing "single filter"
       (is (= [["David" 46]
               ["Brian" 50]]
-             @(fluree/query db '{:select [?name ?age]
-                                 :where  [{:type :ex/User
-                                           :schema/age ?age
-                                           :schema/name ?name}
-                                          [:filter "(> ?age 45)"]]}))))
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  '[?name ?age]
+                                :where   '[{:type        :ex/User
+                                            :schema/age  ?age
+                                            :schema/name ?name}
+                                           [:filter "(> ?age 45)"]]}))))
     (testing "multiple filters on same var"
       (is (= [["David" 46]]
-             @(fluree/query db '{:select [?name ?age]
-                                 :where  [{:type :ex/User
-                                           :schema/age ?age
-                                           :schema/name ?name}
-                                          [:filter "(> ?age 45)" "(< ?age 50)"]]}))))
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  '[?name ?age]
+                                :where   '[{:type        :ex/User
+                                            :schema/age  ?age
+                                            :schema/name ?name}
+                                           [:filter "(> ?age 45)" "(< ?age 50)"]]}))))
     (testing "multiple filters, different vars"
       (is (= [["Brian" "Smith"]]
-             @(fluree/query db '{:select [?name ?last]
-                                 :where  [{:type :ex/User
-                                           :schema/age ?age
-                                           :schema/name ?name
-                                           :ex/last ?last}
-                                          [:filter "(> ?age 45)" "(strEnds ?last \"ith\")"]]}))))
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  '[?name ?last]
+                                :where   '[{:type        :ex/User
+                                            :schema/age  ?age
+                                            :schema/name ?name
+                                            :ex/last     ?last}
+                                           [:filter "(> ?age 45)" "(strEnds ?last \"ith\")"]]}))))
 
     (testing "nested filters"
       (is (= [["Brian" 50]]
-             @(fluree/query db '{:select [?name ?age]
-                                 :where  [{:type :ex/User
-                                           :schema/age ?age
-                                           :schema/name ?name}
-                                          [:filter "(> ?age (/ (+ ?age 47) 2))"]]}))))
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  '[?name ?age]
+                                :where   '[{:type        :ex/User
+                                            :schema/age  ?age
+                                            :schema/name ?name}
+                                           [:filter "(> ?age (/ (+ ?age 47) 2))"]]}))))
 
     ;;TODO: simple-subject-crawl does not yet support filters.
     ;;these are being run as regular analytial queries
     (testing "simple-subject-crawl"
-      (is (= [{:id :ex/david,
-               :type :ex/User,
-               :schema/name "David",
-               :ex/last "Jones",
+      (is (= [{:id           :ex/david,
+               :type         :ex/User,
+               :schema/name  "David",
+               :ex/last      "Jones",
                :schema/email "david@example.org",
-               :schema/age 46,
-               :ex/favNums [15 70],
-               :ex/friend {:id :ex/cam}}
-              {:id :ex/brian,
-               :type :ex/User,
-               :schema/name "Brian",
-               :ex/last "Smith",
+               :schema/age   46,
+               :ex/favNums   [15 70],
+               :ex/friend    {:id :ex/cam}}
+              {:id           :ex/brian,
+               :type         :ex/User,
+               :schema/name  "Brian",
+               :ex/last      "Smith",
                :schema/email "brian@example.org",
-               :schema/age 50,
-               :ex/favNums 7}]
-             @(fluree/query db {:select {"?s" ["*"]}
-                                :where  [{:id "?s", :schema/age "?age"}
-                                         [:filter "(> ?age 45)"]]})))
-      (is (= [{:id :ex/david,
-               :type :ex/User,
-               :schema/name "David",
-               :ex/last "Jones",
+               :schema/age   50,
+               :ex/favNums   7}]
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  {"?s" ["*"]}
+                                :where   [{:id "?s", :schema/age "?age"}
+                                          [:filter "(> ?age 45)"]]})))
+      (is (= [{:id           :ex/david,
+               :type         :ex/User,
+               :schema/name  "David",
+               :ex/last      "Jones",
                :schema/email "david@example.org",
-               :schema/age 46,
-               :ex/favNums [15 70],
-               :ex/friend {:id :ex/cam}}]
-             @(fluree/query db {:select {"?s" ["*"]}
-                                :where  [{:id "?s", :schema/age "?age"}
-                                         [:filter "(> ?age 45)" "(< ?age 50)"]]})))
-      (is (= [{:type :ex/User
+               :schema/age   46,
+               :ex/favNums   [15 70],
+               :ex/friend    {:id :ex/cam}}]
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  {"?s" ["*"]}
+                                :where   [{:id "?s", :schema/age "?age"}
+                                          [:filter "(> ?age 45)" "(< ?age 50)"]]})))
+      (is (= [{:type         :ex/User
                :schema/email "cam@example.org"
-               :ex/favNums [5 10]
-               :schema/age 34
-               :ex/last "Jones"
-               :schema/name "Cam"
-               :id :ex/cam
-               :ex/friend [{:id :ex/brian} {:id :ex/alice}]
-               :ex/favColor "Blue"}]
-             @(fluree/query db {:select {"?s" ["*"]}
-                                :where  [{:id "?s", :ex/favColor "?color"}
-                                         [:filter "(strStarts ?color \"B\")"]]}))))))
+               :ex/favNums   [5 10]
+               :schema/age   34
+               :ex/last      "Jones"
+               :schema/name  "Cam"
+               :id           :ex/cam
+               :ex/friend    [{:id :ex/brian} {:id :ex/alice}]
+               :ex/favColor  "Blue"}]
+             @(fluree/query db {:context [test-utils/default-context
+                                          {:ex "http://example.org/ns/"}]
+                                :select  {"?s" ["*"]}
+                                :where   [{:id "?s", :ex/favColor "?color"}
+                                          [:filter "(strStarts ?color \"B\")"]]}))))))
