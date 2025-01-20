d7a6a5b65fddab6feaed7ea1bfebb8807c914b2d
Benjamin Lamothe <bl@732.one>
2025-01-20 15:00:56 -0500

    update rich-comment to use connect-file api fn

--- dev/user.clj
+++ dev/user.clj
@@ -52,8 +52,8 @@
 
 (comment
 
-  (def file-conn @(fluree/connect {:storage-path "dev/data"
-                                   :defaults     {:did did}}))
+  (def file-conn @(fluree/connect-file {:storage-path "dev/data"
+                                        :defaults     {:did did}}))
 
   (def ledger-alias "user/test")
 
