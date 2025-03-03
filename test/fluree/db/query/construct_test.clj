67419f1a3fcd2f48f730bba8b42d99755bdf7d7d
Daniel Petranek <dpetranek@flur.ee>
2025-03-03 15:48:25 -0600

    support multi-pattern construct clause

--- test/fluree/db/query/construct_test.clj
+++ test/fluree/db/query/construct_test.clj
@@ -4,23 +4,23 @@
             [fluree.db.test-utils :as test-utils]))
 
 (def people-data
-  [{"id"              "ex:jdoe"
-    "type"            "ex:Person"
+  [{"@id"             "ex:jdoe"
+    "@type"           "ex:Person"
     "person:handle"   "jdoe"
     "person:fullName" "Jane Doe"
     "person:favNums"  [3 7 42 99]}
-   {"id"              "ex:bbob"
-    "type"            "ex:Person"
+   {"@id"             "ex:bbob"
+    "@type"           "ex:Person"
     "person:handle"   "bbob"
     "person:fullName" "Billy Bob"
     "person:favNums"  [23]}
-   {"id"              "ex:jbob"
-    "type"            "ex:Person"
+   {"@id"             "ex:jbob"
+    "@type"           "ex:Person"
     "person:handle"   "jbob"
     "person:fullName" "Jenny Bob"
     "person:favNums"  [8 6 7 5 3 0 9]}
-   {"id"              "ex:fbueller"
-    "type"            "ex:Person"
+   {"@id"             "ex:fbueller"
+    "@type"           "ex:Person"
     "person:handle"   "dankeshön"
     "person:fullName" "Ferris Bueller"}])
 
@@ -28,26 +28,37 @@
   (let [conn    @(fluree/connect-memory)
         ledger  @(fluree/create conn "people")
         db0     (fluree/db ledger)
-        context [test-utils/default-str-context
-                 {"person" "http://example.org/Person#"
-                  "ex" "http://example.org/"}]
+        context {"person" "http://example.org/Person#"
+                 "ex" "http://example.org/"}
         db1     @(fluree/stage db0 {"@context" context "insert" people-data})]
     (testing "basic"
       (is (= {"@context" context
               "@graph"
-              [{"@id" "ex:bbob", "label" "Billy Bob"}
-               {"@id" "ex:fbueller", "label" "Ferris Bueller"}
-               {"@id" "ex:jdoe", "label" "Jane Doe"}
-               {"@id" "ex:jbob", "label" "Jenny Bob"}]}
+              [{"@id" "ex:bbob" "label" "Billy Bob"}
+               {"@id" "ex:fbueller" "label" "Ferris Bueller"}
+               {"@id" "ex:jdoe" "label" "Jane Doe"}
+               {"@id" "ex:jbob" "label" "Jenny Bob"}]}
              @(fluree/query db1 {"@context" context
                                  "where" [{"@id" "?s" "person:fullName" "?fullName"}]
                                  "construct" [{"@id" "?s" "label" "?fullName"}]}))))
     (testing "nil context"
       (is (= {"@graph"
-              [{"@id" "http://example.org/bbob", "ex:label" "Billy Bob"}
-               {"@id" "http://example.org/fbueller", "ex:label" "Ferris Bueller"}
-               {"@id" "http://example.org/jdoe", "ex:label" "Jane Doe"}
-               {"@id" "http://example.org/jbob", "ex:label" "Jenny Bob"}]}
+              [{"@id" "http://example.org/bbob" "ex:label" "Billy Bob"}
+               {"@id" "http://example.org/fbueller" "ex:label" "Ferris Bueller"}
+               {"@id" "http://example.org/jdoe" "ex:label" "Jane Doe"}
+               {"@id" "http://example.org/jbob" "ex:label" "Jenny Bob"}]}
              @(fluree/query db1 {"@context" nil
                                  "where" [{"@id" "?s" "http://example.org/Person#fullName" "?fullName"}]
-                                 "construct" [{"@id" "?s" "ex:label" "?fullName"}]}))))))
+                                 "construct" [{"@id" "?s" "ex:label" "?fullName"}]}))))
+    (testing "multiple clauses"
+      (is (= {"@context" {"person" "http://example.org/Person#" "ex" "http://example.org/" "id" "@id"}
+              "@graph"
+              [{"id" "ex:bbob" "name" "Billy Bob" "handle" "bbob"}
+               {"id" "ex:fbueller" "name" "Ferris Bueller" "handle" "dankeshön"}
+               {"id" "ex:jdoe" "name" "Jane Doe" "handle" "jdoe"}
+               {"id" "ex:jbob" "name" "Jenny Bob" "handle" "jbob"}]}
+             @(fluree/query db1 {"@context" (assoc context "id" "@id")
+                                 "where" [{"@id" "?s" "person:fullName" "?fullName"}
+                                          {"@id" "?s" "person:handle" "?handle"}]
+                                 "construct" [{"@id" "?s" "name" "?fullName"}
+                                              {"@id" "?s" "handle" "?handle"}]}))))))
