010f540bcd2dc59b5c0aa6dffaa5c731e5c40fbc
Daniel Petranek <dpetranek@flur.ee>
2025-06-02 16:20:20 -0500

    add test case for explain shape

---
+++ test/fluree/db/explain_test.clj
@@ -0,0 +1,41 @@
+(ns fluree.db.explain-test
+  (:require  [clojure.test :refer [deftest is testing]]
+             [fluree.db.api :as fluree]))
+
+(defn gen-subj
+  [i]
+  {"@id" (str "ex:" i)
+   "ex:foo" "foo"
+   "ex:i" i})
+
+(deftest explain-test
+  (let [conn   @(fluree/connect-memory)
+        db0    @(fluree/create conn "test-explain")
+        db1    @(fluree/stage db0 {"@context" {"ex" "http://example.com/"}
+                                   "insert"
+                                   (mapv (fn [i]
+                                           (cond-> (gen-subj i)
+                                             (odd? i) (assoc "ex:ref1"
+                                                             (cond-> (gen-subj (+ 1000 i))
+                                                               (#{0} (mod i 3))
+                                                               (assoc "ex:ref2" (gen-subj (+ 2000 i))
+                                                                      "ex:ref3" {"@id" (str "ex:" i)})))
+                                             (even? i) (assoc
+                                                         "ex:bar" "bar"
+                                                         "ex:num" [i (inc i) (dec i)])))
+                                         (range 1 100 3))})]
+    (is (= {[:filter "(> 50 ?num)"] [96 48],
+            [:optional {"@id" "?s", "ex:ref1" "?ref1"}] [48 48],
+            [:union {"@id" "?s", "ex:foo" "?str"} {"@id" "?s", "ex:bar" "?str"}] [48 96],
+            {"@id" "?s", "ex:bar" "?str"} [48 48],
+            {"@id" "?s", "ex:foo" "?str"} [48 48],
+            {"@id" "?s", "ex:num" "?num"} [1 48],
+            {"@id" "?s", "ex:ref1" "?ref1"} [48 0]}
+           (:explain @(fluree/explain db1 {"where" [{"@id" "?s" "ex:num" "?num"}
+                                                    ["optional"
+                                                     {"@id" "?s" "ex:ref1" "?ref1"}]
+                                                    ["union"
+                                                     {"@id" "?s" "ex:foo" "?str"}
+                                                     {"@id" "?s" "ex:bar" "?str"}]
+                                                    ["filter" "(> 50 ?num)"]]
+                                           "select" ["?s" "?ref1" "?str"]}))))))
