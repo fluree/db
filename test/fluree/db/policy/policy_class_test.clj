d61e7aaf04f16c34e34dc58565f1446464d507ee
Daniel Petranek <dpetranek@flur.ee>
2025-08-20 23:36:54 -0500

    allow users to specify ledger-specific query opts
    In a dataset, the opts provided with the query (or the override opts) will be applied
    for each ledger in the dataset. Sometimes this is not desirable, for example when you
    want a given policy class to be only used for a subset of the ledgers in a dataset.
    
    This commit introduces ledger-specific opts.
    
    :opts {<ledger-alias> {...(ledger-specific-opts)}}
    
    Right now the only ledger specific opts supported are :policy, :policy-class, and
    :policy-values. Every other opt applies to the query as a whole (:max-fuel, :meta,
    :format, :output, :identity). If a value is specified both under a ledger alias and at
    the top level of an opts map, then the ledger-specific value will override the top-level
    one.

--- test/fluree/db/policy/policy_class_test.clj
+++ test/fluree/db/policy/policy_class_test.clj
@@ -101,3 +101,126 @@
                       "select"   {"?s" ["*"]}
                       "where"    {"@id"   "?s"
                                   "@type" "ex:User"}})))))))))
+
+(deftest dataset-test
+  (let [conn       @(fluree/connect-memory)
+        root-did   (:id (did/private->did-map "8ce4eca704d653dec594703c81a84c403c39f262e54ed014ed857438933a2e1c"))
+        alice-did  (:id (did/private->did-map "c0459840c334ca9f20c257bed971da88bd9b1b5d4fca69d4e3f4b8504f981c07"))
+        _personnel @(fluree/create-with-txn conn {"@context" {"f" "https://ns.flur.ee/ledger#"
+                                                              "ex" "https://example.com/"}
+                                                  "ledger" "test/personnel"
+                                                  "insert"
+                                                  [{"@id" root-did}
+                                                   {"@id" alice-did
+                                                    "f:policyClass" [{"@id" "ex:AuthenticatedPolicy"}]
+                                                    "ex:user" {"@id" "ex:alice"}}
+
+                                                   {"@id" "ex:ssnRestriction"
+                                                    "@type" ["f:AccessPolicy" "ex:AuthenticatedPolicy"]
+                                                    "f:required" true
+                                                    "f:targetProperty" [{"@id" "ex:ssn"}]
+                                                    "f:action" {"@id" "f:view"}
+                                                    "f:query" {"@type" "@json"
+                                                               "@value" {"@context" {"ex" "https://example.com/"}
+                                                                         "where" {"@id" "?$identity"
+                                                                                  "ex:user" {"@id" "?$this"}}}}}
+
+                                                   {"@id" "ex:defaultAllowView"
+                                                    "@type" ["f:AccessPolicy" "ex:UnauthenticatedPolicy"]
+                                                    "f:action" {"@id" "f:view"}
+                                                    "f:query" {"@type" "@json"
+                                                               "@value" {}}}
+
+                                                   {"@id" "ex:alice",
+                                                    "@type" "ex:User",
+                                                    "ex:name" "Alice"
+                                                    "ex:ssn" "111-11-1111"}
+
+                                                   {"@id" "ex:john",
+                                                    "@type" "ex:User",
+                                                    "ex:name" "John"
+                                                    "ex:ssn" "888-88-8888"}]})
+        _widgets @(fluree/create-with-txn conn {"@context" {"f" "https://ns.flur.ee/ledger#"
+                                                            "ex" "https://example.com/"}
+                                                "ledger" "test/widgets"
+                                                "insert"
+                                                [{"@id" root-did}
+                                                 {"@id" alice-did
+                                                  "f:policyClass" [{"@id" "ex:EmployeePolicy"}]
+                                                  "ex:user" {"@id" "ex:alice"}}
+
+                                                 {"@id" "ex:defaultAllowView"
+                                                  "@type" ["f:AccessPolicy" "ex:UnauthenticatedPolicy"]
+                                                  "f:action" {"@id" "f:view"}
+                                                  "f:query" {"@type" "@json"
+                                                             "@value" {}}}
+
+                                                 {"@id" "ex:widget1",
+                                                  "@type" "ex:Product",
+                                                  "ex:name" "Widget1"
+                                                  "ex:salesperson" {"@id" "ex:john"}
+                                                  "ex:price" 99.99
+                                                  "ex:priceCurrency" "USD"}
+                                                 {"@id" "ex:widget2",
+                                                  "@type" "ex:Product",
+                                                  "ex:name" "Widget2"
+                                                  "ex:salesperson" {"@id" "ex:alice"}
+                                                  "ex:price" 990.99
+                                                  "ex:priceCurrency" "USD"}
+                                                 {"@id" "ex:widget3",
+                                                  "@type" "ex:Product",
+                                                  "ex:name" "Widget3"
+                                                  "ex:salesperson" [{"@id" "ex:alice"} {"@id" "ex:john"}]
+                                                  "ex:price" 9.99
+                                                  "ex:priceCurrency" "USD"}]})]
+    (testing "no restrictions"
+      (is (= [["Widget1" "ex:john" "888-88-8888"]
+              ["Widget2" "ex:alice" "111-11-1111"]
+              ["Widget3" "ex:alice" "111-11-1111"]
+              ["Widget3" "ex:john" "888-88-8888"]]
+             @(fluree/query-connection conn {"@context" {"ex" "https://example.com/"}
+                                             "from"     ["test/personnel" "test/widgets"]
+                                             "where"    [{"@id" "?widget" "@type" {"@id" "ex:Product"}}
+                                                         {"@id" "?widget" "ex:name" "?product"}
+                                                         {"@id" "?widget" "ex:salesperson" "?person"}
+                                                         [:optional
+                                                          {"@id" "?person" "ex:name" "?name"}
+                                                          {"@id" "?person" "ex:ssn" "?ssn"}]]
+                                             "select"   ["?product" "?person" "?ssn"]}))))
+    (testing "one policy class for all ledgers"
+      (testing "UnauthenticatedPolicy reveals all"
+        (is (= [["Widget1" "John" "888-88-8888"]
+                ["Widget2" "Alice" "111-11-1111"]
+                ["Widget3" "Alice" "111-11-1111"]
+                ["Widget3" "John" "888-88-8888"]]
+               @(fluree/query-connection conn {"@context" {"ex" "https://example.com/"}
+                                               "from"     ["test/personnel" "test/widgets"]
+                                               "where"    [{"@id" "?widget" "@type" {"@id" "ex:Product"}}
+                                                           {"@id" "?widget" "ex:name" "?product"}
+                                                           {"@id" "?widget" "ex:salesperson" "?person"}
+                                                           [:optional
+                                                            {"@id" "?person" "ex:name" "?name"}
+                                                            {"@id" "?person" "ex:ssn" "?ssn"}]]
+                                               "select"   ["?product" "?name" "?ssn"]}
+                                         {:policy-class ["https://example.com/UnauthenticatedPolicy"]})))))
+
+    (testing "per-ledger policy classes"
+      (testing "only widget data should be visible"
+        (is (= [["Widget1" nil nil]
+                ["Widget2" nil nil]
+                ["Widget3" nil nil]
+                ["Widget3" nil nil]]
+               @(fluree/query-connection conn {"@context" {"ex" "https://example.com/"}
+                                               "from"     ["test/personnel" "test/widgets"]
+                                               "where"    [{"@id" "?widget" "@type" {"@id" "ex:Product"}}
+                                                           {"@id" "?widget" "ex:name" "?product"}
+                                                           {"@id" "?widget" "ex:salesperson" "?person"}
+                                                           [:optional
+                                                            {"@id" "?person" "ex:name" "?name"}
+                                                            {"@id" "?person" "ex:ssn" "?ssn"}]]
+                                               "select"   ["?product" "?name" "?ssn"]}
+                                         {"test/personnel"
+                                          ;; no UnauthenticatedPolicy for personnel
+                                          {:policy-class ["https://example.com/AuthenticatedPolicy"]}
+                                          "test/widgets"
+                                          {:policy-class ["https://example.com/UnauthenticatedPolicy"]}})))))))
