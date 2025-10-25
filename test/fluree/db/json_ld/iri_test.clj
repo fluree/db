64d1dac1520b6d34b5d9ff1a8134b3c110dde076
bplatz <bplatz@gmail.com>
2025-10-25 11:55:39 -0400

    refactor sid-interning tests for clarity and remove interning checks

--- test/fluree/db/json_ld/iri_test.clj
+++ test/fluree/db/json_ld/iri_test.clj
@@ -3,38 +3,32 @@
             [fluree.db.json-ld.iri :as iri]))
 
 (deftest sid-interning-test
-  (testing "SID interning when enabled"
-    (when (iri/interning-enabled?)
-      (testing "returns identical instances for same ns-code and name"
-        (let [sid1 (iri/deserialize-sid [8 "test-subject"])
-              sid2 (iri/deserialize-sid [8 "test-subject"])]
-          (is (identical? sid1 sid2)
-              "Same SID should return identical instance")))
+  (testing "deserialize-sid returns identical instances for same ns-code and name"
+    (let [sid1 (iri/deserialize-sid [8 "test-subject"])
+          sid2 (iri/deserialize-sid [8 "test-subject"])]
+      (is (identical? sid1 sid2)
+          "Interning ensures identical instances for same SID values")))
 
-      (testing "returns different instances for different SIDs"
-        (let [sid1 (iri/deserialize-sid [8 "subject-1"])
-              sid2 (iri/deserialize-sid [8 "subject-2"])]
-          (is (not (identical? sid1 sid2))
-              "Different SIDs should not be identical")))
+  (testing "deserialize-sid returns different instances for different SIDs"
+    (let [sid1 (iri/deserialize-sid [8 "subject-1"])
+          sid2 (iri/deserialize-sid [8 "subject-2"])]
+      (is (not (identical? sid1 sid2))
+          "Different SID values should not be identical")))
 
-      (testing "interner cache tracks entries"
-        (let [initial-size (iri/interner-size)]
-          ;; Create a unique SID
-          (iri/deserialize-sid [8 (str "unique-" (System/currentTimeMillis))])
-          (let [new-size (iri/interner-size)]
-            (is (>= new-size initial-size)
-                "Cache size should increase or stay same after creating SID"))))))
+  (testing "iri->sid returns identical instances for same IRI"
+    (let [sid1 (iri/iri->sid "https://ns.flur.ee/ledger#test")
+          sid2 (iri/iri->sid "https://ns.flur.ee/ledger#test")]
+      (is (identical? sid1 sid2)
+          "Interning ensures identical instances for same IRI")))
 
-  (testing "SID creation when interning disabled"
-    (when-not (iri/interning-enabled?)
-      (testing "deserialize-sid still works without interning"
-        (let [sid (iri/deserialize-sid [8 "test-subject"])]
-          (is (some? sid))
-          (is (= 8 (iri/get-ns-code sid)))
-          (is (= "test-subject" (iri/get-name sid))))))))
+  (testing "interning works across different creation methods"
+    (let [sid-from-deserialize (iri/deserialize-sid [8 "example"])
+          sid-from-iri (iri/iri->sid "https://ns.flur.ee/ledger#example")]
+      (is (identical? sid-from-deserialize sid-from-iri)
+          "Same SID created via different methods should be identical"))))
 
 (deftest sid-equality-test
-  (testing "SID equality is value-based regardless of interning"
+  (testing "SID equality is value-based"
     (let [sid1 (iri/deserialize-sid [8 "subject"])
           sid2 (iri/deserialize-sid [8 "subject"])]
       (is (= sid1 sid2) "Equal SIDs should be equal")
